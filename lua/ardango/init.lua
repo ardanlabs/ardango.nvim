abd79530a75654baa1b7e9fc340a9d31fbd26171
cesarFuhr <cesar.cara@protonmail.com>
2022-08-31 14:39:03 -0300

    added org imports

--- lua/ardango/init.lua
+++ lua/ardango/init.lua
@@ -1,5 +1,5 @@
 local tsutils = require('nvim-treesitter.ts_utils')
-local ui = require('ui')
+local ui = require('ardango.ui')
 
 local M = {}
 
@@ -68,5 +68,22 @@ M.BuildCurrPackage = function()
     end,
   })
 end
+
+-- OrgImports is a function to update imports of a buffer.
+M.OrgBufImports = function(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
 
 return M
